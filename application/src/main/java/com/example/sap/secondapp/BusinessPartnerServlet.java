e68df9dec2cb854cefcd7f1e1a410ef447e95ddf
Ritu <aigal.ritu@gmail.com>
2018-07-04 10:57:52 +0000

    tests

---
+++ application/src/main/java/com/example/sap/secondapp/BusinessPartnerServlet.java
@@ -0,0 +1,70 @@
+package com.example.sap.secondapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+
+/**
+ * Servlet implementation class BusinessPartnerServlet
+ */
+@WebServlet("/BusinessPartnerServlet")
+public class BusinessPartnerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = CloudLoggerFactory.getLogger(BusinessPartnerServlet.class);
+
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BusinessPartnerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+            final List<BusinessPartner> businessPartners =
+                    new GetBusinessPartnersCommand().execute();
+
+            response.setContentType("application/json");
+            response.getWriter().write(new Gson().toJson(businessPartners));
+
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(e.getMessage());
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
